6bd451441b4028b5d14f0ddef9e2ddd6b1f18a4c
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-14 14:29:33 +0200

    feat: init project

---
+++ js-biglietto-treno.js
@@ -0,0 +1,17 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va stampato in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+Questo richiederà un minimo di ricerca.
+*/
+
+/* DATI
+//  Numero di chilometri
+//  Età del passeggero
+//  Prezzo del biglietto 0.21euro al km
+*/
+
+// ESECUZIONE LOGICA
+
